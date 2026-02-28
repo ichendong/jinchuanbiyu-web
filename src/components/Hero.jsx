51a0d8a8428f461757b60109c8c8c1b8e1a1a386
Ichen Tung <jimdong89211@gmail.com>
2026-02-28 20:29:08 +0800

    style: Hero 文字調整

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,8 @@ const Hero = () => {
       <div className="hero-content">
         <p className="hero-label">Est. 2023 • Seoul Soul in Tainan</p>
         <h1 className="font-serif hero-title">
-          <span>時光裡的</span>
-          <span className="sub">溫潤碧玉</span>
+          <span>金波粼川</span>
+          <span>碧色溫玉</span>
         </h1>
         <p className="hero-desc">
           在奶油色的午後，品嚐一杯傳承家族記憶的特調。<br/>極簡，是為了讓心更靠近。
